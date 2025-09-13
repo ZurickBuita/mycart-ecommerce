b7e04b3f03f17ea42b177c888d81108c9e83597d
ZurickBuita <koenjiquenzie@gmail.com>
2025-09-13 12:16:59 +0800

    Update category features

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $fillable = [
-        "", 
-        ""
+        "name", 
+        "slug",
+        "visibility",
+        "description"
     ];
 }
