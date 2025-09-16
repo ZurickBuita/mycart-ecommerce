92a7cd29f936eae6d41b97eb2d645d1ff3bb9de5
ZurickBuita <koenjiquenzie@gmail.com>
2025-09-16 15:15:40 +0800

    Add address relationship

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -24,4 +24,9 @@ class Order extends Model
     {
         return $this->hasMany(OrderItem::class);
     }
+
+    public function address()
+    {
+        return $this->hasOne(Address::class);
+    }
 }
