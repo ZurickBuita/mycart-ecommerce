7a43745bee0656780d6976e86eff7c466751197b
ZurickBuita <koenjiquenzie@gmail.com>
2025-09-16 15:13:43 +0800

    Add address modal and database

---
+++ app/Models/Address.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'phone',
+        'street_address',
+        'city',
+        'state',
+        'zip_code',
+        'order_id',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
