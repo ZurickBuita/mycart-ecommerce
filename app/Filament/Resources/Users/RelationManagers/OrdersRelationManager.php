557a24806800aedf4d6dc9119c98967335276dcf
ZurickBuita <koenjiquenzie@gmail.com>
2025-09-20 06:58:37 +0800

    Update order table columns

--- app/Filament/Resources/Users/RelationManagers/OrdersRelationManager.php
+++ app/Filament/Resources/Users/RelationManagers/OrdersRelationManager.php
@@ -2,9 +2,12 @@
 
 namespace App\Filament\Resources\Users\RelationManagers;
 
+use App\Enums\OrderStatus;
 use App\Filament\Resources\Orders\OrderResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables\Columns\SelectColumn;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class OrdersRelationManager extends RelationManager
@@ -16,6 +19,20 @@ class OrdersRelationManager extends RelationManager
     public function table(Table $table): Table
     {
         return $table
+            ->columns([
+                TextColumn::make('id')
+                    ->label('Order ID')
+                    ->sortable(),
+                TextColumn::make('payment_method'),
+                TextColumn::make('payment_status')
+                    ->badge(),
+                SelectColumn::make('status')
+                    ->options(OrderStatus::class),
+                TextColumn::make('currency')
+                    ->formatStateUsing(fn(string $state) => strtoupper($state)),
+                TextColumn::make('created_at')
+                    ->label('Order Date'),
+            ])
             ->headerActions([
                 CreateAction::make(),
             ]);
