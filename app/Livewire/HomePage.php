a4dbcc1f6c2360ea374a457f136cc1a03bbdf53c
ZurickBuita <koenjiquenzie@gmail.com>
2025-09-26 20:43:55 +0800

    Turn brand section data into dynamic

--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -2,14 +2,19 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
-#[Title('Homepage - MyCart')] 
+#[Title('Homepage - MyCart')]
 class HomePage extends Component
 {
     public function render()
     {
-        return view('livewire.home-page');
+        $brands = Brand::where('is_active', 1)->get();
+
+        return view('livewire.home-page', [
+            'brands' => $brands
+        ]);
     }
 }
