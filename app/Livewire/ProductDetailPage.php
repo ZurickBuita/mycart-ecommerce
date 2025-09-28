6e605b19451e7dcbf8f77b7bfb31a7329bf394a4
ZurickBuita <koenjiquenzie@gmail.com>
2025-09-28 20:28:29 +0800

    Add quantity increment, and decrement functionality; and add to cart function

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -2,7 +2,10 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartManagement;
+use App\Livewire\Partials\Navbar;
 use App\Models\Product;
+use Jantinnerezo\LivewireAlert\Facades\LivewireAlert;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
@@ -10,6 +13,30 @@ use Livewire\Component;
 class ProductDetailPage extends Component
 {
     public $slug;
+    public $quantity = 1;
+
+    public function increaseQty()
+    {
+        $this->quantity++;
+    }
+    public function decreaseQty()
+    {
+        if ($this->quantity > 1)
+            $this->quantity--;
+    }
+
+    public function addToCart($product_id)
+    {
+        $total_count = CartManagement::addItemToCart($product_id);
+        $this->dispatch('update-cart-count', total_count: $total_count)->to(Navbar::class);
+
+        LivewireAlert::text('Product added to the cart successfully!')
+            ->success()
+            ->position('bottom-end')
+            ->toast()
+            ->timer(3000)
+            ->show();
+    }
 
     public function mount($slug)
     {
