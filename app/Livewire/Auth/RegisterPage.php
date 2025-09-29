0b594fe7cba5bc63a8df589925e845dab24948e2
ZurickBuita <koenjiquenzie@gmail.com>
2025-09-29 21:39:41 +0800

    Add register functionality

--- app/Livewire/Auth/RegisterPage.php
+++ app/Livewire/Auth/RegisterPage.php
@@ -2,12 +2,38 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
 #[Title('Register - MyCart')]
 class RegisterPage extends Component
 {
+    public $name;
+    public $email;
+    public $password;
+
+    public function save()
+    {
+        $this->validate([
+            "name" => "required",
+            "email" => "required|email|unique:users|max:255",
+            "password" => "required|min:6|max:255"
+        ]);
+
+        $user = User::create([
+            "name" => $this->name,
+            "email" => $this->email,
+            "password" => Hash::make($this->password)
+        ]);
+
+        // login user
+        auth()->login($user);
+
+        // redirect to home page
+        return redirect()->intended();
+    }
     public function render()
     {
         return view('livewire.auth.register-page');
